1f76df6055132008b79b744db7d16073fcf1ce7a
gopi_i <30035910+gopi-i@users.noreply.github.com>
2022-03-22 02:26:44 +0530

    Add files via upload
    add day1 learnings that explain printing output in console using c & rust.

---
+++ c_rust_day_1/c_postgre_help.c
@@ -0,0 +1,22 @@
+/*
+ * printf() function prototype is available in 
+ * stdio.h header file. 
+ * Here, we are including header file using include macro
+ */
+#include <stdio.h>
+
+/*
+ * Entry point of the C program is main function.
+ * Using return type as int as per modern C convention
+ */
+int
+main() {
+
+    /*
+     * printf(...) is used to print the data on console.
+     * Here, we are printing raw string.
+     */
+    printf("postgres is the PostgreSQL server.\n\n");
+
+    return 0;
+}
---
+++ c_rust_day_1/rust_postgre_help.rs
@@ -0,0 +1,14 @@
+/*
+ * In rust, we have entry point of program as `fn main()`
+ */
+
+fn main() {
+    
+    /*
+     * println! is macro which is used to print the data.
+     * If we omit `!` in `println!` then it will be treated as function
+     * and compiler tries to find out the function in program.
+     */
+    println!("postgres is the PostgreSQL server.\n\n");
+
+}
