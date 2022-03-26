b3a453ff674c9da3f9e0c7fe4c8af0bb845b6539
gopi_i <30035910+gopi-i@users.noreply.github.com>
2022-03-27 01:45:38 +0530

    Add files via upload
    add learning about for loop in C and Rust

---
+++ c_rust_day_6/postgres_for.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int
+main() {
+
+	int sublist_length = 10;
+
+	for(int i = 0; i < sublist_length; i++) {
+    
+    	printf("%d\n", i);
+    }
+    
+    return 0;
+    
+}
---
+++ c_rust_day_6/postgres_for.rs
@@ -0,0 +1,9 @@
+fn main() {
+
+	let sublist_length: i32 = 10;
+    
+    for i in (0..sublist_length).step_by(1) {
+    i	println!("{}", i);
+    }
+    
+}
