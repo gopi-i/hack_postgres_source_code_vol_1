17ebf17c75f0ccdf74d7aad2a1cd82d6b96e3664
gopi_i <30035910+gopi-i@users.noreply.github.com>
2022-04-01 03:59:02 +0530

    Add files via upload
    add learning about rust ownership system - ownership

---
+++ rust_day_11/not_str_type.rs
@@ -0,0 +1,7 @@
+fn main() {
+
+    let _base: i32 = 1;
+    let _taker: i32 = _base;
+
+    println!("{}", _base);
+}
---
+++ rust_day_11/str_type.rs
@@ -0,0 +1,8 @@
+fn main() {
+
+    let _str: String = String::from("String Objects");
+    let _taker: String = _str;
+
+    println!("{}", _str);
+
+}
