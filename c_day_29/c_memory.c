902e433a63f4b400d05a0e35dd374489699df9a2
gopi_i <30035910+gopi-i@users.noreply.github.com>
2022-04-19 23:34:14 +0530

    Add files via upload
    add learning about static scoping in C

---
+++ c_day_29/c_memory.c
@@ -0,0 +1,14 @@
+//main-line-0
+int _global = 0;
+
+//main-line-2
+int main() {
+
+	// main-line-3
+	static int _static_local;
+   
+	int _local = 1;
+    
+    return 0;
+}
+
---
+++ c_day_29/c_static_scoping.c
@@ -0,0 +1,24 @@
+// main-line-0
+int add(int x, int y);
+
+// main-line-1
+int x = 1;
+
+// main-line-2
+int main() {
+
+	int x;
+    int y = 2;
+    
+    // main-line-3
+    int z = add(x,y);
+    
+}
+
+// add-line-4
+int add(int x, int y) {
+
+	// add-line-5
+	return (x+y);
+    
+}
